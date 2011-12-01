70c6d3cc143a8485ef66ba78d22ea0de0f6af3f3
Nestor Bohorquez <tca7410nb@gmail.com>
2011-12-01 21:57:30 +0000

    git-svn-id: http://jarcob:8080/svn/Paris/LineaBase@2 fc7e63f8-4f90-8241-a1ba-5c3e8a827fe3

---
+++ ServidorValeria/IStock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Zuliaworks.Netzuela.Paris.ServidorValeria
+{
+    [ServiceContract]
+    public interface IStock
+    {
+        [OperationContract]
+        Stock GetStock(string Symbol);
+    }
+    
+    [DataContract]
+    public class Stock
+    {
+        [DataMember]
+        public string Symbol { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        [DataMember]
+        public string Company { get; set; }
+        [DataMember]
+        public decimal Close { get; set; }
+    }
+}
---
+++ ServidorValeria/Stocks.svc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Zuliaworks.Netzuela.Paris.ServidorValeria
+{
+    public class Stocks : IStock
+    {
+        #region Implementacion de interfaces
+
+        public Stock GetStock(string Symbol)
+        {
+            Stock st = null;
+            switch (Symbol.ToUpper())
+            {
+                case "GOOG":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Google Inc.",
+                        Close = 495
+                    };
+                    break;
+                case "MSFT":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Microsoft Corporation",
+                        Close = 25
+                    };
+                    break;
+                case "YHOO":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Yahoo! Inc.",
+                        Close = 17
+                    };
+                    break;
+                case "AMZN":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Amazon.com, Inc.",
+                        Close = 92
+                    };
+                    break;
+            }
+            return st;
+        }
+
+        #endregion
+    }
+}
