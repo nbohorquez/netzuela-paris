de35580c4329fb5933bae4666b06c1e490bb52d4
Nestor Bohorquez <tca7410nb@gmail.com>
2012-02-25 16:27:10 -0430

    Empece a usar la biblioteca css Bootstrap. Agregue funcionalidad basica para los views de productos y tiendas.

--- src/Paris/paris/models.py
+++ src/Paris/paris/models.py
@@ -2,16 +2,27 @@ from sqlalchemy import MetaData, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker, mapper
 from zope.sqlalchemy import ZopeTransactionExtension
+from .spuria import TABLAS_A_OBJETOS
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-# Aqui van las clases
-class Acceso(object):
-	pass
-
 # Asocia las tablas de la base de datos con clases en python
-def cargar_tablas(engine):
-	metadata = MetaData(engine)
-	moz_acceso = Table('acceso', metadata, autoload=True)
-	mapper(Acceso, moz_acceso)
+def cargar_tablas(motor):
+	metadata = MetaData(motor)
+	for asociacion in TABLAS_A_OBJETOS:
+		tabla = asociacion['tabla']
+		objeto = asociacion['objeto']
+		esquema_tabla = Table(tabla, metadata, autoload=True)
+		mixIn(objeto, [object])
+		mapper(globals()[objeto], esquema_tabla)
+	
+# Codigo tomado de: http://danielkaes.wordpress.com/2009/07/30/create-new-classes-with-python-at-runtime/	
+def mixIn(classname, parentclasses):
+	if len(parentclasses) > 0:
+		parents = map(lambda p:p.__name__, parentclasses)
+		createclass = "class %s (%s):\n\tpass" % (classname, ",".join(parents))
+	else:
+		createclass = "class %s:\n\tpass" % classname
+	exec createclass
+	globals()[classname] = eval(classname)
--- src/Paris/paris/views.py
+++ src/Paris/paris/views.py
@@ -1,7 +1,40 @@
 from pyramid.view import view_config
-from .models import DBSession, Acceso
+from pyramid.httpexceptions import ( 
+	HTTPFound,
+    HTTPNotFound,
+)
+from .models import (
+	DBSession, 
+	Tienda, 
+	Producto, 
+	Cliente,
+)
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-	accesos = DBSession.query(Acceso).all()
-	return {'accesos': accesos}
+# Aptana siempre va a decir que las clases de Spuria (Tienda, Producto, etc) no estan 
+# definidas explicitamente en ninguna parte. Lo que ocurre es que yo las cargo de forma 
+# dinamica cuando inicia la aplicacion.
+
+@view_config(route_name='inicio', renderer='plantillas/inicio.pt')
+def inicio_view(peticion):
+	return { 'nombre_pagina': 'Inicio' }
+
+@view_config(route_name='producto', renderer='plantillas/producto.pt')
+def producto_view(peticion):
+	producto_id = peticion.matchdict['producto_id']
+	producto = DBSession.query(Producto).filter_by(ProductoID = producto_id).first()
+	
+	if producto is None:
+		return HTTPNotFound('Vos lo que estais es loco! Esa verga no existe')
+	
+	return { 'nombre_pagina': 'Producto', 'producto': producto }
+
+@view_config(route_name='tienda', renderer='plantillas/tienda.pt')
+def tienda_view(peticion):
+	tienda_id = peticion.matchdict['tienda_id']
+	tienda = DBSession.query(Tienda).filter_by(TiendaID = tienda_id).first()
+	
+	if tienda is None:
+		return HTTPNotFound('Vos lo que estais es loco! Esa verga no existe')
+	
+	cliente = DBSession.query(Cliente).filter_by(RIF = tienda.Cliente_P).first()
+	return { 'nombre_pagina': 'Tienda', 'tienda_id': tienda.TiendaID, 'cliente': cliente}
