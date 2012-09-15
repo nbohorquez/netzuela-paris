47cd06931ac1dc52889a35a0c286d33f9f0a7f6d
Nestor Bohorquez <tca7410nb@gmail.com>
2012-09-15 00:06:46 -0430

    Cambios menores en crear_app.sh para que hacerla mas legible.

--- bin/crear_app.sh
+++ bin/crear_app.sh
@@ -1,7 +1,19 @@
-#/bin/bash
+#!/bin/bash
 
-wsgi_load="echo LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so >> /etc/apache2/mods-available/wsgi.load"
-wsgi_conf="cat > /etc/apache2/mods-available/wsgi.conf << EOF
+sitio_dummy="/etc/apache2/sites-enabled/000-default"
+dir_log="/var/log/netzuela"
+dir_simbolico_www="/var/www/paris"
+paris_available="/etc/apache2/sites-available/paris"
+paris_enabled="/etc/apache2/sites-enabled/paris"
+redireccion_available="/etc/apache2/sites-available/redireccion"
+redireccion_enabled="/etc/apache2/sites-enabled/redireccion"
+wsgi_load_available="/etc/apache2/mods-available/wsgi.load"
+wsgi_load_enabled="/etc/apache2/mods-enabled/wsgi.load"
+wsgi_conf_available="/etc/apache2/mods-available/wsgi.conf"
+wsgi_conf_enabled="/etc/apache2/mods-enabled/wsgi.conf"
+mod_wsgi_so="/usr/lib/apache2/modules/mod_wsgi.so"
+wsgi_load="echo LoadModule wsgi_module $mod_wsgi_so >> $wsgi_load_available"
+wsgi_conf="cat > $wsgi_conf_available << EOF
 WSGIApplicationGroup %{GLOBAL}
 WSGIPassAuthorization On
 EOF
@@ -13,32 +25,32 @@ from pyramid.paster import get_app
 application = get_app(production_ini, 'main')
 EOF
 "
-apache_paris="cat > /etc/apache2/sites-available/paris << EOF
+apache_paris="cat > $paris_available << EOF
 <VirtualHost *:80>
         ServerName www.netzuela.com
         ServerAdmin tca7410nb@gmail.com
 
-        WSGIDaemonProcess pyramid processes=1 threads=4 python-path=/var/www/paris/lib/python2.7/site-packages
-        WSGIScriptAlias / /var/www/paris/pyramid.wsgi
+        WSGIDaemonProcess pyramid processes=1 threads=4 python-path=$dir_simbolico_www/lib/python2.7/site-packages
+        WSGIScriptAlias / $dir_simbolico_www/pyramid.wsgi
 
-        <Directory /var/www/paris>
+        <Directory $dir_simbolico_www>
                 WSGIProcessGroup pyramid
                 Order allow,deny
                 Allow from all
         </Directory>
 
-        #ErrorLog ${APACHE_LOG_DIR}/paris_error.log
-        ErrorLog /var/log/netzuela/paris_error.log
+        #ErrorLog \${APACHE_LOG_DIR}/paris_error.log
+        ErrorLog $dir_log/paris_error.log
         # Possible values include: debug, info, notice, warn, error, crit,
         # alert, emerg.
         LogLevel warn
 
-        #CustomLog ${APACHE_LOG_DIR}/paris_access.log combined
-        CustomLog /var/log/netzuela/paris_access.log combined
+        #CustomLog \${APACHE_LOG_DIR}/paris_access.log combined
+        CustomLog $dir_log/paris_access.log combined
 </VirtualHost>
 EOF
 "
-apache_redireccion="cat > /etc/apache2/sites-available/redireccion << EOF
+apache_redireccion="cat > $redireccion_available << EOF
 <VirtualHost *:80>
         ServerName redireccion.netzuela.com
         ServerAlias netzuela.com
@@ -47,7 +59,7 @@ apache_redireccion="cat > /etc/apache2/sites-available/redireccion << EOF
 EOF
 "
 
-crear_env() {
+crear_env () {
         virtualenv --no-site-packages --distribute env
         rm *.tar.gz
         source env/bin/activate
@@ -58,7 +70,7 @@ crear_env() {
         cd ../../bin
 }
 
-instalar_mod_wsgi() {
+instalar_mod_wsgi () {
         dir=`pwd`
         cd /tmp
         wget https://modwsgi.googlecode.com/files/mod_wsgi-3.4.tar.gz
@@ -73,44 +85,44 @@ instalar_mod_wsgi() {
         cd "$dir"
 }
 
-crear_archivo_wsgi_load() {
-        if [ ! -f /etc/apache2/mods-available/wsgi.load ]; then
+crear_archivo_wsgi_load () {
+        if [ ! -f "$wsgi_load_available" ]; then
                 bash -c "$wsgi_load"
         fi
-        ln -s /etc/apache2/mods-available/wsgi.load /etc/apache2/mods-enabled/wsgi.load
+        ln -s "$wsgi_load_available" "$wsgi_load_enabled"
 }
 
-crear_archivo_wsgi_conf() {
-        if [ ! -f /etc/apache2/mods-available/wsgi.conf ]; then
+crear_archivo_wsgi_conf () {
+        if [ ! -f "$wsgi_conf_available" ]; then
                 bash -c "$wsgi_conf"
         fi
-        ln -s /etc/apache2/mods-available/wsgi.conf /etc/apache2/mods-enabled/wsgi.conf
+        ln -s "$wsgi_conf_available" "$wsgi_conf_enabled"
 }
-crear_archivo_pyramid_wsgi() {
+crear_archivo_pyramid_wsgi () {
         bash -c "$pyramid_wsgi"
         chmod 755 env/pyramid.wsgi
 }
 
-crear_production_ini() {
+crear_archivo_production_ini () {
         ln -s `pwd`/../src/paris/production.ini env/production.ini
 }
 
-crear_archivo_apache() {
-        if [ ! -f /etc/apache2/sites-available/redireccion ]; then
+crear_archivo_apache () {
+        if [ ! -f "$paris_available" ]; then
                 bash -c "$apache_paris"
         fi
-        ln -s /etc/apache2/sites-available/paris /etc/apache2/sites-enabled/paris
+        ln -s "$paris_available" "$paris_enabled"
 }
 
-crear_archivo_redireccion() {
-        if [ ! -f /etc/apache2/sites-available/redireccion ]; then
+crear_archivo_redireccion () {
+        if [ ! -f "$redireccion_available" ]; then
                 bash -c "$apache_redireccion"
         fi
-        ln -s /etc/apache2/sites-available/redireccion /etc/apache2/sites-enabled/redireccion
+        ln -s "$redireccion_available" "$redireccion_enabled"
 }
 
-configurar_var_www() {
-        ln -s `pwd`/env/ /var/www/paris
+configurar_var_www () {
+        ln -s `pwd`/env/ "$dir_simbolico_www"
 }
 
 if [ "$USER" != "root" ]; then
@@ -125,19 +137,19 @@ if [ ! -f env/bin/python ]; then
 fi
 echo "Ambiente virtual creado"
 
-if [ ! -f /usr/lib/apache2/modules/mod_wsgi.so ]; then
+if [ ! -f "$mod_wsgi_so" ]; then
         echo "mod_wsgi no esta instalado, instalando..."
         instalar_mod_wsgi
 fi
 echo "mod_wsgi instalado"
 
-if [ ! -f /etc/apache2/mods-enabled/wsgi.load ]; then
+if [ ! -f "$wsgi_load_enabled" ]; then
         echo "El archivo wsgi.load no existe, creandolo..."
         crear_archivo_wsgi_load
 fi
 echo "wsgi.load creado"
 
-if [ ! -f /etc/apache2/mods-enabled/wsgi.conf ]; then
+if [ ! -f "$wsgi_conf_enabled" ]; then
         echo "El archivo wsgi.conf no existe, creandolo..."
         crear_archivo_wsgi_conf
 fi
@@ -151,29 +163,29 @@ echo "pyramid.wsgi creado"
 
 if [ ! -f env/production.ini ]; then
         echo "El archivo production.ini no existe, creandolo..."
-        crear_production_ini
+        crear_archivo_production_ini
 fi
 echo "production.ini creado"
 
-if [ -f /etc/apache2/sites-enabled/000-default ]; then
-        echo "El archivo 000-default existe, borrandolo..."
-        rm /etc/apache2/sites-enabled/000-default
+if [ -f "$sitio_dummy" ]; then
+        echo "El sitio por defecto existe, borrandolo..."
+        rm "$sitio_dummy" 
 fi
-echo "000-default borrado"
+echo "sitio por defecto borrado"
 
-if [ ! -f /etc/apache2/sites-enabled/paris ]; then
+if [ ! -f "$paris_enabled" ]; then
         echo "El archivo paris no existe, creandolo..."
         crear_archivo_apache
 fi
 echo "paris creado"
 
-if [ ! -f /etc/apache2/sites-enabled/redireccion ]; then
+if [ ! -f "$redireccion_enabled" ]; then
         echo "El archivo redireccion no existe, creandolo..."
         crear_archivo_redireccion
 fi
 echo "redireccion creado"
 
-if [ ! -L /var/www/paris ]; then
+if [ ! -L "$dir_simbolico_www" ]; then
         echo "El directorio /var/www/ no esta configurado, trabajando..."
         configurar_var_www
 fi
