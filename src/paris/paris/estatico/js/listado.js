85c2be315fc9547ac9137c17464b30054eac5994
Nestor Bohorquez <tca7410nb@gmail.com>
2012-09-24 13:27:19 -0430

    Ahora se cambia la region geografica al hacer clic sobre un poligono en el mapa.

--- src/paris/paris/estatico/js/listado.js
+++ src/paris/paris/estatico/js/listado.js
@@ -6,4 +6,11 @@ $(document).ready(function () {
     var terr_padre = $('input[name=territorio_padre]').val();
     var terr_yo = $('input[name=territorio_id]').val();
     $('#mapa').dibujar_niveles(terr_padre, terr_yo);
+    $('#mapa').bind('poligono_click', function (e, data) {
+        var remitente = data.remitente;
+        var listado = $('input[name=listado]').val().toLowerCase();
+        var categoria = $('input[name=categoria_id]').val();
+        window.location.href = '/' + listado + '/cat' + categoria 
+                                + 'geo' + remitente.id;
+    });
 });
